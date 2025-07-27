6561b37afef43431bc62b44c5a73f214b6162316
Eslam Qadri <eslamqadri@Eslams-MacBook-Pro.local>
2025-07-27 23:08:25 +0300

    update user profile

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from user_profile.models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "mobile_number",
+        "date_joined",
+        "is_booked",
+        "is_email_verified",
+        "is_mobile_verified",
+        "date_of_birth",
+    )
+    search_fields = ("username", "email", "mobile_number")
+    list_filter = ("is_booked", "is_email_verified", "is_mobile_verified")
+    ordering = ("-date_joined",)
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from user_profile.vadiateros import mobile_number_regex
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+import uuid
 
 
-# Create your models here.
-class UserProfile(User):
-    pk = models.UUIDField(primary_key=True)
+class UserProfile(AbstractUser):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField("Username", max_length=150, unique=True, db_index=True)
+    email = models.EmailField("Email", unique=True, db_index=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField("First Name", max_length=30, blank=True)
+    last_name = models.CharField("Last Name", max_length=30, blank=True)
+    full_name = models.CharField("Full Name", max_length=60, blank=True)
 
     image = models.ImageField(upload_to="profile_images/", blank=True, null=True)
     mobile_number = models.CharField(
@@ -16,6 +22,10 @@ class UserProfile(User):
     is_email_verified = models.BooleanField(default=False)
     is_mobile_verified = models.BooleanField(default=False)
     date_of_birth = models.DateField(blank=True, null=True)
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
         return f"{self.username}"
