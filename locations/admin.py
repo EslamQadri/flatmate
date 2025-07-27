c4d936836fa48a42a95583e3d0e8c3f5df39900b
EslamQadri <eslamkadryuwk@gmail.com>
2025-07-27 23:34:37 +0300

    add locations tables

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from locations.models import Country, City, Area
+
 
 # Register your models here.
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name", "code")
+    search_fields = ("name", "code")
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "country")
+    search_fields = ("name", "country__name")
+    list_filter = ("country",)
+
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ("name", "city")
+    search_fields = ("name", "city__name")
+    list_filter = ("city",)
+    ordering = ("city", "name")
+    prepopulated_fields = {"name": ("city",)}
--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    code = models.CharField(max_length=3, unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(
+        Country, related_name="cities", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.name}, {self.country.name}"
+
+
+class Area(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, related_name="areas", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.city.name}"
